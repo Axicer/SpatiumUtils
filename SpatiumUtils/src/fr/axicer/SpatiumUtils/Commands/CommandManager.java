6a126eb290d9dea1ea53bf262a6dfb50fe979146
Axicer <Julien@pc-Julien>
2015-07-23 17:18:20 +0200

    Added /sun and /rain

--- SpatiumUtils/src/fr/axicer/SpatiumUtils/Commands/CommandManager.java
+++ SpatiumUtils/src/fr/axicer/SpatiumUtils/Commands/CommandManager.java
@@ -13,12 +13,14 @@ import fr.axicer.SpatiumUtils.Commands.CommandExecutors.MoonCommand;
 import fr.axicer.SpatiumUtils.Commands.CommandExecutors.NameCommand;
 import fr.axicer.SpatiumUtils.Commands.CommandExecutors.NightCommand;
 import fr.axicer.SpatiumUtils.Commands.CommandExecutors.PokeCommand;
+import fr.axicer.SpatiumUtils.Commands.CommandExecutors.RainCommand;
 import fr.axicer.SpatiumUtils.Commands.CommandExecutors.SbanCommand;
 import fr.axicer.SpatiumUtils.Commands.CommandExecutors.SbanUUIDCommand;
 import fr.axicer.SpatiumUtils.Commands.CommandExecutors.SbankCommand;
 import fr.axicer.SpatiumUtils.Commands.CommandExecutors.SkickCommand;
 import fr.axicer.SpatiumUtils.Commands.CommandExecutors.SmuteCommand;
 import fr.axicer.SpatiumUtils.Commands.CommandExecutors.SreloadCommand;
+import fr.axicer.SpatiumUtils.Commands.CommandExecutors.SunCommand;
 import fr.axicer.SpatiumUtils.Commands.CommandExecutors.SunbanCommand;
 import fr.axicer.SpatiumUtils.Commands.CommandExecutors.SunbanUUIDCommand;
 import fr.axicer.SpatiumUtils.Commands.CommandExecutors.SunmuteCommand;
@@ -48,5 +50,7 @@ public class CommandManager {
 		pl.getCommand("gravitygun").setExecutor(new GravityGunCommand());
 		pl.getCommand("day").setExecutor(new DayCommand());
 		pl.getCommand("night").setExecutor(new NightCommand());
+		pl.getCommand("sun").setExecutor(new SunCommand());
+		pl.getCommand("rain").setExecutor(new RainCommand());
 	}
 }
